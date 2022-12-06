60f073f43baa1b77fd9d69ddc33463a4f8f82a5f
ashesash <aishwarya.s@live.com>
2022-12-06 23:15:48 +1100

    change to author in fixture

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -5,7 +5,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'content']
+        fields = ['title', 'pub_date', 'content']
         widgets = {
             'pub_date': forms.DateTimeInput(format=('%d%m/%Y %H:%M'), 
             attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-
+USER = get_user_model()
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    # author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        USER, on_delete=models.CASCADE
+    )
     pub_date = models.DateTimeField()
     # pub_time = models.TimeField() 
     content = models.TextField()
