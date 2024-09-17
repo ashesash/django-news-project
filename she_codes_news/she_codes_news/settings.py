80cb7f2cdc2e5ce99f8f0b7a43f245e5bdbc23db
ashesash <ashsahu@outlook.com>
2024-09-18 01:04:29 +1000

    css changes

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -27,10 +27,11 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(
-    'DJANGO_DEBUG', 
-    'False'
-) != 'False'
+# DEBUG = os.environ.get(
+#     'DJANGO_DEBUG', 
+#     'False'
+# ) != 'False'
+DEBUG = True
 
 STATIC_URL = '/static/'
 
--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("news/", include("news.urls")),
+    path('', include("news.urls")),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
